5c768e32e18758bae02e6f773806094721172215
Alex Martin <alex@analogmemory.xyz>
2023-05-28 22:04:19 -0400

    change uri again

--- src/algos/just-cats.ts
+++ src/algos/just-cats.ts
@@ -2,7 +2,7 @@ import { InvalidRequestError } from '@atproto/xrpc-server'
 import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
 
-export const uri = 'at://did:web:bsky.brainworms.work/app.bsky.feed.generator/test-just-cats'
+export const uri = 'at://did:plc:7kmdkdlcr3wg3gwjr2gvhgp4/app.bsky.feed.generator/test-just-cats'
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   let builder = ctx.db
